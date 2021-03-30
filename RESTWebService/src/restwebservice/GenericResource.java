5503a509f2c83fb77f946e670c287fbc71ea0fb6
Hasani Holder <Hasani.Holder@gov.ky>
2021-03-30 11:37:35 -0500

    Initial commit

---
+++ RESTWebService/src/restwebservice/GenericResource.java
@@ -0,0 +1,34 @@
+package restwebservice;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("restwebservice")
+@Consumes("application/json")
+@Produces("application/json")
+public class GenericResource {
+    public GenericResource() {
+    }
+
+    @POST
+    public Response postData(String content) {
+
+        // Provide method implementation.
+        // TODO
+
+        throw new UnsupportedOperationException();
+    }
+
+    @GET
+    public String getData() {
+
+        // Provide method implementation.
+        // TODO
+
+        throw new UnsupportedOperationException();
+    }
+}
