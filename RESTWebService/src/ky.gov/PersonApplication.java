68966e323f7b5030e3b55f5ce7a56ac0a5476002
Hasani Holder <Hasani.Holder@gov.ky>
2021-03-31 13:17:59 -0500

    Reworked project to use Maven

--- RESTWebService/src/ky.gov/PersonApplication.java
+++ RESTWebService/src/ky.gov/PersonApplication.java
@@ -1,18 +1,16 @@
-package restwebservice;
+package ky.gov;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
 
 @ApplicationPath("resources")
-public class GenericApplication extends Application {
+public class PersonApplication extends javax.ws.rs.core.Application {
     public Set<Class<?>> getClasses() {
         Set<Class<?>> classes = new HashSet<Class<?>>();
 
         // Register root resources.
-        classes.add(GenericResource.class);
 
         // Register provider classes.
 
